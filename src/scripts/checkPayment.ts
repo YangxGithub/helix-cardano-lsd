5b2d296dad34ace026f67a1672fb6f949054872f
HavenCross <havenquan@qq.com>
2023-10-11 11:55:06 +0800

    change ip

--- src/scripts/checkPayment.ts
+++ src/scripts/checkPayment.ts
@@ -4,7 +4,7 @@ import { Response } from 'node-fetch';
 
 const walletId: string = "101b3814d6977de4b58c9dedc67b87c63a4f36dd";
 const totalExpectedLovelace: number = 1000000;
-const resp: Response = await fetch(`http://localhost:9998/v2/wallets/${walletId}`);
+const resp: Response = await fetch(`http://127.0.0.1:9998/v2/wallets/${walletId}`);
 const wallet: any = await resp.json();
 const balance: number = wallet.balance.total.quantity;
 
